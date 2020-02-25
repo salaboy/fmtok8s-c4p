d73c20e10b9fffde55a9a0e89a63844959941b6b
salaboy <salaboy@gmail.com>
2020-02-25 16:37:38 +0000

    adding ENV var check

--- src/test/resources/contracts/shouldAcceptPostProposal.groovy
+++ src/test/resources/contracts/shouldAcceptPostProposal.groovy
@@ -1,22 +1,37 @@
 import org.springframework.cloud.contract.spec.Contract
-Contract.make {
-    description "should accept POST with new Proposal"
-    request{
-        method 'POST'
-        url '/'
-        body([
-            "title": "",
-            "description": "",
-            "author": "",
-            "email": ""
-        ])
-        headers {
-            contentType('application/json')
+[
+    Contract.make {
+        name "should accept POST with new Proposal"
+        request{
+            method 'POST'
+            url '/'
+            body([
+                "title": "",
+                "description": "",
+                "author": "",
+                "email": ""
+            ])
+            headers {
+                contentType('application/json')
+            }
+        }
+        response {
+            status OK()
+        }
+    },
+    Contract.make {
+        name "should return info for service"
+        request{
+            method 'GET'
+            url '/info'
+
+            headers {
+                contentType('application/json')
+            }
+        }
+        response {
+            status OK()
+            body("C4P v" + ((System.getenv("VERSION")!=null)?System.getenv("VERSION"):"0.0.0"))
         }
     }
-    response {
-
-        status OK()
-
-    }
-}
+]
