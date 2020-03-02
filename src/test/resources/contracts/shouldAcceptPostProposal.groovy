a1cae9a380de1545aa64f214e846b2d231246a4a
salaboy <salaboy@gmail.com>
2020-03-02 14:10:06 +0000

    removing info test

--- src/test/resources/contracts/shouldAcceptPostProposal.groovy
+++ src/test/resources/contracts/shouldAcceptPostProposal.groovy
@@ -28,20 +28,6 @@ import org.springframework.cloud.contract.spec.Contract
                     "email": $(anyEmail())
             )
         }
-    },
-    Contract.make {
-        name "should return info for service"
-        request{
-            method 'GET'
-            url '/info'
-
-            headers {
-                contentType('application/json')
-            }
-        }
-        response {
-            status OK()
-            body("C4P v" + ((System.getenv("VERSION")!=null)?System.getenv("VERSION"):"0.0.0"))
-        }
     }
+
 ]
