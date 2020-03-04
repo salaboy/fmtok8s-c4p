65a948557f043492f6f249b389ecfcab0f7439fe
salaboy <salaboy@gmail.com>
2020-03-04 12:02:28 +0000

    removing lazy loading for spring beans

--- src/main/java/com/salaboy/conferences/c4p/DemoApplication.java
+++ src/main/java/com/salaboy/conferences/c4p/DemoApplication.java
@@ -1,23 +1,9 @@
 package com.salaboy.conferences.c4p;
 
-import com.salaboy.conferences.c4p.model.AgendaItem;
-import com.salaboy.conferences.c4p.model.Proposal;
-import com.salaboy.conferences.c4p.model.ProposalDecision;
-import com.salaboy.conferences.c4p.model.ProposalStatus;
-// import io.zeebe.spring.client.EnableZeebeClient;
-// import io.zeebe.spring.client.ZeebeClientLifecycle;
-// import io.zeebe.spring.client.annotation.ZeebeDeployment;
 import io.zeebe.spring.client.EnableZeebeClient;
 import io.zeebe.spring.client.annotation.ZeebeDeployment;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.*;
 
 @SpringBootApplication
 @EnableZeebeClient
