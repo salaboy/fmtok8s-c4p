a84e9f792f5e36555bbdd9f56fff16b42e8b1f39
Matheus Henrique de Sousa Cruz <matheus.cruz@radixeng.com.br>
2020-10-09 11:41:45 -0300

    Solving the NPE on Contract test

--- src/test/java/com/salaboy/conferences/c4p/ContractVerifierBase.java
+++ src/test/java/com/salaboy/conferences/c4p/ContractVerifierBase.java
@@ -1,10 +1,12 @@
 package com.salaboy.conferences.c4p;
 
 import io.restassured.RestAssured;
+import io.zeebe.spring.util.ZeebeAutoStartUpLifecycle;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,6 +17,9 @@ public abstract class ContractVerifierBase {
 
     @LocalServerPort
     int port;
+
+    @MockBean
+    private ZeebeAutoStartUpLifecycle zeebeAutoStartUpLifecycle;
 
     @Before
     public void setup() {
