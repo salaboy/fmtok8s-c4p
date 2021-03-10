533819defa564ac63db14dd546e15458fc538986
salaboy <salaboy@gmail.com>
2021-03-10 10:53:33 +0000

    updating serviceinfo endpoint and deployment info

--- src/main/java/com/salaboy/conferences/c4p/InfoController.java
+++ src/main/java/com/salaboy/conferences/c4p/InfoController.java
@@ -1,5 +1,6 @@
 package com.salaboy.conferences.c4p;
 
+import com.salaboy.conferences.c4p.model.ServiceInfo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,24 @@ public class InfoController {
     @Value("${version:0.0.0}")
     private String version;
 
+    @Value("${POD_ID:}")
+    private String podId;
+
+    @Value("${POD_NODE_NAME:}")
+    private String podNodeName;
+
+    @Value("${POD_NAMESPACE:}")
+    private String podNamespace;
+
+    //@TODO: add to EndPoint Actuator to leverage Spring Boot architecture
     @GetMapping("/info")
-    public String infoWithVersion() {
-        return "{ \"name\" : \"C4P Service\", \"version\" : \"v" + version + "\", \"source\": \"https://github.com/salaboy/fmtok8s-c4p/releases/tag/v"+version+"\" }";
+    public ServiceInfo getInfo() {
+        return new ServiceInfo(
+                "C4P Service (REST)",
+                "v"+version,
+                "https://github.com/salaboy/fmtok8s-c4p/releases/tag/v" + version,
+                podId,
+                podNamespace,
+                podNodeName);
     }
 }
